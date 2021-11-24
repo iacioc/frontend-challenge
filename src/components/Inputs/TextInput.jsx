2d8ceb0673333d852a30a68b603d09d43b37d522
Ioan Cioca <ioancioca@gmail.com>
2021-11-24 17:30:26 -0500

    Removed leftover comments

--- src/components/Inputs/TextInput.jsx
+++ src/components/Inputs/TextInput.jsx
@@ -6,19 +6,12 @@ const StyledInput = styled.input`
   height: 1.5rem;
 `;
 
-const TextInput = ({ label, ...otherProps }) => {
-  // const [field] = useField(otherProps);
-  // const { errors, touched } = useFormikContext();
-  // const fieldName = otherProps.name;
-  // const hasErrorAndTouched = errors[fieldName] && touched[fieldName];
-
-  return (
-    <HiddenLabelInput
-      baseComponent={<StyledInput />}
-      label={label}
-      {...otherProps}
-    />
-  );
-};
+const TextInput = ({ label, ...otherProps }) => (
+  <HiddenLabelInput
+    baseComponent={<StyledInput />}
+    label={label}
+    {...otherProps}
+  />
+);
 
 export default TextInput;
